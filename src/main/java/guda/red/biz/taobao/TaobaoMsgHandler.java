78c7cf1693ea5e33b6578b71b953eea9e84dea8f
foodoon <foodoon@qq.com>
2015-02-11 11:59:57 +0800

    addd

---
+++ src/main/java/guda/red/biz/taobao/TaobaoMsgHandler.java
@@ -0,0 +1,31 @@
+package guda.red.biz.taobao;
+
+import com.taobao.api.internal.tmc.Message;
+import com.taobao.api.internal.tmc.MessageHandler;
+import com.taobao.api.internal.tmc.MessageStatus;
+import com.taobao.api.internal.tmc.TmcClient;
+
+/**
+ * Created by foodoon on 2015/2/8.
+ */
+public class TaobaoMsgHandler  {
+
+    public void startListener(){
+
+        TmcClient client = new TmcClient("app_key", "app_secret","default");
+        client.setMessageHandler(new MessageHandler() {
+            public void onMessage(Message message, MessageStatus status) {
+                try {
+                    System.out.println(message.getContent());
+                    System.out.println(message.getTopic());
+                    // 默认不抛出异常则认为消息处理成功
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    status.fail();// 消息处理失败回滚，服务端需要重发
+                }
+            }
+        });
+        client.connect();
+
+    }
+}
