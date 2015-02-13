3ecabbc0563711addeb9a056db37a02c4bc71844
foodoon <foodoon@qq.com>
2015-02-13 13:30:14 +0800

    add

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,7 +1,7 @@
 cd .. && git pull
-tomcat_home=/home/ec2-user/tomcat-task
+tomcat_home=/dev_data/server/tomcat-red
 mvn clean package -Dmaven.test.skip=true -Ppro
-mv target/task-1.0-SNAPSHOT.war ROOT.war
+mv target/red-1.0-SNAPSHOT.war ROOT.war
 rm -rf ${tomcat_home}/webapps/ROOT.war
 rm -rf ${tomcat_home}/webapps/ROOT
 
