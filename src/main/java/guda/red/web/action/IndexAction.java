e6bdb8a4b891b8a8ccef649d7410c33bf281eacf
foodoon <foodoon@qq.com>
2015-02-13 14:46:44 +0800

    add

--- src/main/java/guda/red/web/action/IndexAction.java
+++ src/main/java/guda/red/web/action/IndexAction.java
@@ -4,6 +4,8 @@ import guda.red.common.security.AppContexHolder;
 import guda.red.dao.MsgNoticeDOMapper;
 import guda.red.dao.domain.MsgNoticeDO;
 import guda.red.dao.domain.MsgNoticeDOCriteria;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -20,6 +22,7 @@ import java.util.List;
 @Controller
 public class IndexAction {
 
+    protected final Logger log = LoggerFactory.getLogger(getClass());
     @Autowired
     private MsgNoticeDOMapper msgNoticeDOMapper;
 
@@ -27,23 +30,25 @@ public class IndexAction {
     public String list(HttpServletRequest request, ModelMap modelMap) {
         MsgNoticeDOCriteria msgNoticeDOCriteria = new MsgNoticeDOCriteria();
         msgNoticeDOCriteria.createCriteria().andTaobaoSellerIdEqualTo(AppContexHolder.getContext().getUserProfile().getTaobaoSellerDO().getId());
-
-        List<MsgNoticeDO> msgNoticeDOs = msgNoticeDOMapper.selectByExample(msgNoticeDOCriteria);
-        if(msgNoticeDOs.size() == 0){
-            MsgNoticeDO msgNoticeDO = new MsgNoticeDO();
-            msgNoticeDO.setRefund(0);
-            msgNoticeDO.setWaitPay(0);
-            msgNoticeDO.setPositiveComment(0);
-            msgNoticeDO.setSign(0);
-            msgNoticeDO.setRecvItem(0);
-            msgNoticeDO.setSendItem(0);
-            msgNoticeDO.setTaobaoSellerId(AppContexHolder.getContext().getUserProfile().getTaobaoSellerDO().getId());
-            msgNoticeDO.setGmtCreated(new Date());
-            msgNoticeDOMapper.insert(msgNoticeDO);
-            modelMap.put("msgNotice",msgNoticeDO);
-        }
-        else if(msgNoticeDOs.size()>0){
-            modelMap.put("msgNotice",msgNoticeDOs.get(0));
+        try {
+            List<MsgNoticeDO> msgNoticeDOs = msgNoticeDOMapper.selectByExample(msgNoticeDOCriteria);
+            if (msgNoticeDOs.size() == 0) {
+                MsgNoticeDO msgNoticeDO = new MsgNoticeDO();
+                msgNoticeDO.setRefund(0);
+                msgNoticeDO.setWaitPay(0);
+                msgNoticeDO.setPositiveComment(0);
+                msgNoticeDO.setSign(0);
+                msgNoticeDO.setRecvItem(0);
+                msgNoticeDO.setSendItem(0);
+                msgNoticeDO.setTaobaoSellerId(AppContexHolder.getContext().getUserProfile().getTaobaoSellerDO().getId());
+                msgNoticeDO.setGmtCreated(new Date());
+                msgNoticeDOMapper.insert(msgNoticeDO);
+                modelMap.put("msgNotice", msgNoticeDO);
+            } else if (msgNoticeDOs.size() > 0) {
+                modelMap.put("msgNotice", msgNoticeDOs.get(0));
+            }
+        }catch(Exception e){
+            log.error("",e);
         }
         return "index.vm";
     }
